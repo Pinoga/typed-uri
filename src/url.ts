792e2fd8ee1f5c7904b8ddff3e9bf800f606f424
Fernando Bizzotto <fbizzotto@blackpointcyber.com>
2025-10-25 14:02:27 +0100

    add reserved and unreserved chars

--- src/url.ts
+++ src/url.ts
@@ -51,19 +51,77 @@ export type Letter =
   | "X"
   | "Y";
 
-export type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
-
 export type Plus = "+";
 export type Hyphen = "-";
 export type Dot = ".";
 export type Underscore = "_";
+export type Colon = ":";
+export type AtSign = "@";
+export type Percent = "%";
 export type Tilde = "~";
 export type DoubleSlash = "//";
 export type Slash = "/";
 export type QuestionMark = "?";
 export type NumberSign = "#";
+export type ExclamationMark = "!";
+export type DollarSign = "$";
+export type Ampersand = "&";
+export type SingleQuote = "'";
+export type LeftParenthesis = "(";
+export type RightParenthesis = ")";
+export type Asterisk = "*";
+export type PlusSign = "+";
+export type Comma = ",";
+export type SemiColon = ";";
+export type EqualSign = "=";
+export type LeftSquareBracket = "[";
+export type RightSquareBracket = "]";
+
+export type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+
+export type HexDigit =
+  | "a"
+  | "b"
+  | "c"
+  | "d"
+  | "e"
+  | "f"
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | Digit;
+
+export type GenericDelimiter =
+  | Colon
+  | Slash
+  | QuestionMark
+  | NumberSign
+  | LeftSquareBracket
+  | RightSquareBracket
+  | AtSign;
+
+export type SubDelimiter =
+  | ExclamationMark
+  | DollarSign
+  | AtSign
+  | SingleQuote
+  | LeftParenthesis
+  | RightParenthesis
+  | Asterisk
+  | PlusSign
+  | Comma
+  | SemiColon
+  | EqualSign;
 
 export type URIUnreserved = Letter | Digit | Hyphen | Dot | Underscore | Tilde;
+export type URIReserved = GenericDelimiter | SubDelimiter;
+
+export type PercentEncoded = `${Percent}${HexDigit}${HexDigit}`;
+
+export type PChar = URIUnreserved | PercentEncoded | Colon | AtSign;
 
 export type URISchemeRest<T extends string> = T extends
   | Letter
