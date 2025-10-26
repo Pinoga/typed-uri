9ab04377a9d24c3771f2e7f5a245405e6d6f07db
Fernando Bizzotto <fbizzotto@blackpointcyber.com>
2025-10-26 19:53:06 +0000

    add N to M occurrences of Hex16 util

--- src/types/ip.ts
+++ src/types/ip.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { DecOctet, Hex16Bits } from "./aliases";
-import type { Decrement } from "./utils";
+import type { Decrement, Increment } from "./utils";
 
 export type IPv4<T extends string> =
   T extends `${infer Octet1}.${infer Octet2}.${infer Octet3}.${infer Octet4}`
@@ -24,19 +24,48 @@ type Hex32<T extends string> =
 
 type Hex16AndColon<T extends string> = `${Hex16Bits<T>}:`;
 
-type UpToNHex16<T extends string, N extends number, Start = true> = N extends 0
+type NToMHex16<
+  T extends string,
+  N extends number,
+  M extends number,
+  Acc extends number = 0,
+  Start = true,
+> = M extends never
   ? never
   : T extends ""
     ? Start extends true
-      ? T
-      : never
-    : T extends `${Hex16Bits<infer _S>}:`
-      ? T
-      : T extends `${Hex16Bits<infer _S>}:${infer Rest}`
-        ? Rest extends UpToNHex16<Rest, Decrement[N], false>
-          ? T
-          : never
-        : never;
+      ? N extends 0
+        ? T
+        : never
+      : Acc extends N
+        ? T
+        : never
+    : T extends `${Hex16Bits<infer _S>}:${infer Rest}`
+      ? Rest extends NToMHex16<
+          Rest,
+          N,
+          Decrement[M],
+          Acc extends N ? Acc : Increment[Acc],
+          false
+        >
+        ? T
+        : never
+      : never;
+
+type _ = NToMHex16<"", 0, 1>;
+type _ = NToMHex16<"", 0, 2>;
+type _ = NToMHex16<"", 1, 1>;
+type _ = NToMHex16<"0000:", 0, 1>;
+type _ = NToMHex16<"0000:", 1, 1>;
+type _ = NToMHex16<"0000:", 1, 2>;
+type _ = NToMHex16<"0000:0000:", 1, 1>;
+type _ = NToMHex16<"0000:0000:", 1, 2>;
+type _ = NToMHex16<"0000:0000:", 2, 2>;
+type _ = NToMHex16<"0000:0000:0000", 2, 2>;
+type _ = NToMHex16<"0000:0000:0000", 2, 3>;
+type _ = NToMHex16<"0000:0000:0000:", 0, 3>;
+type _ = NToMHex16<"0000:0000:0000:", 1, 3>;
+type _ = NToMHex16<"0000:0000:0000:", 2, 3>;
 
 type NHex16<T extends string, N extends number> = N extends 0
   ? never
@@ -45,16 +74,20 @@ type NHex16<T extends string, N extends number> = N extends 0
       ? T
       : never
     : T extends `${Hex16Bits<infer _S>}:${infer Rest}`
-      ? Rest extends UpToNHex16<Rest, Decrement[N]>
+      ? Rest extends NHex16<Rest, Decrement[N]>
         ? T
         : never
       : never;
 
 export type IPv6<T extends string> = T extends `${infer Left}::${infer Right}`
   ? Left extends ""
-    ? Right extends `${UpToNHex16<infer H16, 6>}${Hex32<infer H32>}`
-      ? T
-      : never
+    ? ExtractAfterLast<Right, ":"> extends IPv4<infer _>
+      ? ExtractUntilLast<Right, ":"> extends NToMHex16<infer _, 1, 6>
+        ? T
+        : never
+      : `${Right}:` extends NToMHex16<infer _, 1, 8>
+        ? T
+        : never
     : 1
   : T extends `${infer H16_1}:${infer H16_2}:${infer H16_3}:${infer H16_4}:${infer H16_5}:${infer H16_6}:${infer H32}`
     ? Hex32<H32> extends never
@@ -74,6 +107,48 @@ export type IPv6<T extends string> = T extends `${infer Left}::${infer Right}`
         : never
     : never;
 
-type A = "0000:0000" extends `${UpToNHex16<infer H16, 6>}${Hex32<infer H32>}`
-  ? `${H16}${H32}`
-  : never;
+type RR =
+  "0000:0000:0000:0000:0000:0000:127.0.0.1" extends `${string}${IPv4<infer V4>}`
+    ? V4
+    : never;
+
+type ExtractUntilLast<
+  T extends string,
+  C extends string,
+  Start = true,
+> = T extends `${infer Prefix}${C}${infer Suffix}`
+  ? `${Prefix}${C}${ExtractUntilLast<Suffix, C, false>}`
+  : Start extends true
+    ? never
+    : "";
+
+type ExtractAfterLast<
+  T extends string,
+  C extends string,
+  Start = true,
+> = T extends `${infer _}${C}${infer Suffix}`
+  ? ExtractAfterLast<Suffix, C, false>
+  : Start extends true
+    ? never
+    : T;
+
+type _ = ExtractUntilLast<"::0000:0000:0000:0000:0000:0000:127.0.0.1", ":">;
+type _ = ExtractUntilLast<"0000", ":">;
+type _ = ExtractAfterLast<"::0000:0000:0000:0000:0000:0000:127.0.0.1", ":">;
+type _ = ExtractAfterLast<"0000", ":">;
+
+type _ = IPv6<"::0000:0000:0000:0000:0000:0000:127.0.0.1">;
+type _ = "0000:0000" extends `${infer H16}${infer Rest}` ? Rest : never;
+
+type _ = "0:0000" extends `${H<infer Rest>}${Hex16Bits<infer _>}`
+  ? true
+  : false;
+
+type _ = "0000:0000" extends `${infer U}${Hex16Bits<infer _>}` ? true : false;
+
+type _ =
+  "0000:0000" extends `${NToMHex16<infer H16, 7>}${Hex16Bits<infer H16_Final>}`
+    ? true
+    : false;
+type _ = "0000" extends `${Hex16Bits<infer H16>}` ? true : false;
+type _ = "0000:" extends NToMHex16<infer H16, 7> ? true : false;
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,2 @@
 export type Decrement = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+export type Increment = [1, 2, 3, 4, 5, 6, 7, 8, 9, never];
